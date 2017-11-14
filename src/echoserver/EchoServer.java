24d3e6c78086025cc283010bf14c74597eea014d
root <root@DESKTOP-FK3SQFB.localdomain>
2017-11-14 15:10:57 -0600

    added some more code, Have more of an idea on how to use runnable now. still need to use run, which will take a number of imputs

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -17,19 +17,27 @@ public class EchoServer {
 	}
 
 	private void start() throws IOException, InterruptedException {
-		//Add a pool somehow
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+		//  Creates a thread pool that creates new threads as needed, but will reuse previously constructed threads when they are available, and uses the provided ThreadFactory to create new threads when needed.
+		ExecutorService pools = new Executors.newCachedThreadPool() ;
+		
 		while (true) {
-			Socket socket = serverSocket.accept();
-			InputStream inputStream = socket.getInputStream();
-			OutputStream outputStream = socket.getOutputStream();
-			int b;
-			while ((b = inputStream.read()) != -1) {
-				outputStream.write(b);
+			// Needs sockets from the CachedPool.
+			Socket impactSocket = serverSocket.accept();
+			//need to point to an Runnalbe thing...
 			}
 		}
-	}
+	
+	
 	//Looks like implements Runnable is a possible route. Needs a run, and each run will
 	//be its own thread. So while loop needs to changed in start, and most over into the 
 	//new class.
+
+//Socket socket = serverSocket.accept();
+//InputStream inputStream = socket.getInputStream();
+//OutputStream outputStream = socket.getOutputStream();
+//int b;
+//while ((b = inputStream.read()) != -1) {
+//	outputStream.write(b);
+//}
 }
